78e67306fecc49f3f1e0f34ffa6f1c471ab04a03
ara <ara@test.com>
2019-06-17 23:21:42 -0400

    Edited data.service.spec.ts

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DateService } from './data.service';
+import { DataService } from './data.service';
 
-describe('DateService', () => {
+describe('DataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: DateService = TestBed.get(DateService);
+    const service: DataService = TestBed.get(DataService);
     expect(service).toBeTruthy();
   });
 });
